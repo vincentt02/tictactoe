6d95a5cc1ec5668bc8912846b326a2815528f403
Vincent <90940496+vincentt02@users.noreply.github.com>
2022-01-29 18:31:57 -0600

    bot player added

--- script.js
+++ script.js
@@ -12,16 +12,6 @@ const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
 statusDisplay.innerHTML = currentPlayerTurn();
 
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-  gameState[clickedCellIndex] = currentPlayer;
-  clickedCell.innerHTML = currentPlayer;
-}
-
-function handlePlayerChange() {
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-  statusDisplay.innerHTML = currentPlayerTurn();
-}
-
 const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -32,6 +22,73 @@ const winningConditions = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
+function botPlays() {
+  let canWin = false;
+  while (currentPlayer === "O" && gameActive) {
+    let avaliableChoices = [];
+    for (let i = 0; i < gameState.length; i++) {
+      if (gameState[i] !== "X" && gameState[i] !== "O") {
+        avaliableChoices.push(i);
+      }
+    }
+    let winChoice;
+    for (let i = 0; i <= 7; i++) {
+      const winCondition = winningConditions[i];
+      let a = gameState[winCondition[0]];
+      let b = gameState[winCondition[1]];
+      let c = gameState[winCondition[2]];
+
+      if (a === "O" && b === "O" && c !== "X") {
+        winChoice = winCondition[2];
+        canWin = true;
+        /*
+        console.log(winCondition);
+        console.log(`winning choice avaliable picked ${choice}`);
+        */
+        break;
+      } else if (b === "O" && c === "O" && a !== "X") {
+        winChoice = winCondition[0];
+        canWin = true;
+        /*
+        console.log(winCondition);
+        console.log(`winning choice avaliable picked ${choice}`);
+        */
+        break;
+      } else if (a === "O" && c === "O" && b !== "X") {
+        winChoice = winCondition[1];
+        canWin = true;
+        /*
+        console.log(winCondition);
+        console.log(`winning choice avaliable picked ${choice}`);
+        */
+        break;
+      }
+      //console.log("no winning choice");
+    }
+
+    if (canWin) {
+      document.querySelector(`[data-cell-index="${winChoice}"`).click();
+    } else {
+      choice =
+        avaliableChoices[Math.floor(Math.random() * avaliableChoices.length)];
+      console.log(avaliableChoices);
+      console.log(choice);
+      document.querySelector(`[data-cell-index="${choice}"`).click();
+    }
+  }
+}
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+  gameState[clickedCellIndex] = currentPlayer;
+  clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  statusDisplay.innerHTML = currentPlayerTurn();
+  botPlays();
+}
 
 function handleResultValidation() {
   let roundWon = false;
